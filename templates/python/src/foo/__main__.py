7a1cbac1a6bb9b366b0b9f3dc387966422abefbf
octvs <octvs@posteo.de>
2025-04-30 13:16:13 +0200

    feat: python: enable dev mode on template

--- templates/python/src/foo/__main__.py
+++ templates/python/src/foo/__main__.py
@@ -1,5 +1,12 @@
 from foo import hello_world
 
+# Make CLI runnable from source tree with `python src/package`
+if not __package__:
+    import sys
+    from pathlib import Path
+
+    sys.path.insert(0, str(Path(__file__).parent.parent))
+
 
 def main() -> None:
     print(hello_world())
