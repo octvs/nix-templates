78905e9bf7707ad71713f18872021944714d1d2a
octvs <octvs@posteo.de>
2026-06-14 23:11:06 +0200

    feat: add nix fmt defn for python template

--- templates/python/flake.nix
+++ templates/python/flake.nix
@@ -5,10 +5,15 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} ({...}: {
+      imports = [inputs.treefmt-nix.flakeModule];
       systems = import inputs.systems;
       perSystem = {pkgs, ...}: let
         dependencies = with pkgs.python3.pkgs; [numpy];
@@ -20,6 +25,13 @@
           src = ./.;
           build-system = with pkgs.python3.pkgs; [setuptools];
           inherit dependencies;
+        };
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs.ruff-format = {
+            enable = true;
+            lineLength = 79;
+          };
         };
       };
     });
