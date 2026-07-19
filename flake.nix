ad404bbe87b370a4e63ac2937088536b52aeb6e7
octvs <octvs@posteo.de>
2026-07-19 15:23:57 +0200

    docs: add description for each template

--- flake.nix
+++ flake.nix
@@ -9,13 +9,34 @@
   in {
     packages.${sys} = import ./pkgs pkgs;
     templates = {
-      doortag.path = ./templates/tex/doortag;
-      eisvogel.path = ./templates/tex/eisvogel;
-      letter.path = ./templates/tex/letter;
-      python.path = ./templates/python;
-      typst-base.path = ./templates/typst/base;
-      typst-letter.path = ./templates/typst/letter;
-      typst-name-tag.path = ./templates/typst/name-tag;
+      doortag = {
+        path = ./templates/tex/doortag;
+        description = "template for making tags via tex";
+      };
+      eisvogel = {
+        path = ./templates/tex/eisvogel;
+        description = "template for documents via eisvogel tex template";
+      };
+      letter = {
+        path = ./templates/tex/letter;
+        description = "template for writing letter via tex";
+      };
+      python = {
+        path = ./templates/python;
+        description = "template for python project";
+      };
+      typst-base = {
+        path = ./templates/typst/base;
+        description = "template for bare typst project";
+      };
+      typst-letter = {
+        path = ./templates/typst/letter;
+        description = "template for writing letter via typst";
+      };
+      typst-name-tag = {
+        path = ./templates/typst/name-tag;
+        description = "template for making tags via typst";
+      };
     };
   };
 }
