0f494f461e6d6f1e0a57f42d321c4c0510dde6a1
octvs <octvs@posteo.de>
2026-07-19 15:28:12 +0200

    build: migrate main flake file to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,42 +1,42 @@
 {
   description = "nix templates for convenience";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
 
-  outputs = {nixpkgs, ...}: let
-    sys = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${sys};
-  in {
-    packages.${sys} = import ./pkgs pkgs;
-    templates = {
-      doortag = {
-        path = ./templates/tex/doortag;
-        description = "template for making tags via tex";
-      };
-      eisvogel = {
-        path = ./templates/tex/eisvogel;
-        description = "template for documents via eisvogel tex template";
-      };
-      letter = {
-        path = ./templates/tex/letter;
-        description = "template for writing letter via tex";
-      };
-      python = {
-        path = ./templates/python;
-        description = "template for python project";
-      };
-      typst-base = {
-        path = ./templates/typst/base;
-        description = "template for bare typst project";
-      };
-      typst-letter = {
-        path = ./templates/typst/letter;
-        description = "template for writing letter via typst";
-      };
-      typst-name-tag = {
-        path = ./templates/typst/name-tag;
-        description = "template for making tags via typst";
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      flake.templates = {
+        doortag = {
+          path = ./templates/tex/doortag;
+          description = "template for making tags via tex";
+        };
+        eisvogel = {
+          path = ./templates/tex/eisvogel;
+          description = "template for documents via eisvogel tex template";
+        };
+        letter = {
+          path = ./templates/tex/letter;
+          description = "template for writing letter via tex";
+        };
+        python = {
+          path = ./templates/python;
+          description = "template for python project";
+        };
+        typst-base = {
+          path = ./templates/typst/base;
+          description = "template for bare typst project";
+        };
+        typst-letter = {
+          path = ./templates/typst/letter;
+          description = "template for writing letter via typst";
+        };
+        typst-name-tag = {
+          path = ./templates/typst/name-tag;
+          description = "template for making tags via typst";
+        };
       };
     };
-  };
 }
